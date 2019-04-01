aa42e238cf890ffa5ebdc4b4155e8b151009cb3c
Alejandro Saucedo <axsauze@gmail.com>
2019-04-01 16:30:48 +0100

    updates on 1

--- 1_install_basic.sh
+++ 1_install_basic.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-export USER=alejandro
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as root"
+    exit
+fi
 
 sudo apt install -y git curl 
+# Working from personal laptop so cache git password
+git config --global user.name "Alejandro Saucedo"
+git config --global user.email "axsauze@gmail.com"
+git config --global credential.helper wincred
 
 # Install node
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
@@ -12,10 +19,9 @@ export NVM_DIR="$HOME/.nvm"
 nvm install node
 nvm alias default node
 
-
 # Install zsh
 sudo apt install zsh -y
-sudo chsh -s `which zsh` $USER
+sudo chsh -s `which zsh` alejandro
 
 # Install Oh my zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh 
@@ -26,8 +32,8 @@ sudo apt install silversearcher-ag -y
 # ripgrep
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb 
 sudo dpkg -i ripgrep_0.10.0_amd64.deb 
-# fzf
+# fzf (This will ask for options)
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf 
-~/.fzf/install
+yes | ~/.fzf/install
 
 
